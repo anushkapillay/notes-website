7ff60e68c793ca5f2688d5bdb427ed835160d6ef
anushka pillay <93579943+anushkapillay@users.noreply.github.com>
2025-01-18 21:34:23 +0530

    index.js

--- NotesJS/index.js
+++ NotesJS/index.js
@@ -1,53 +1,65 @@
-let myLeads = []; // `makes a string`
-// myLeads =  JSON.stringify(myLeads); //sends data as string
-// myLeads =  JSON.parse(myLeads); // sends data as object
-// myLeads.push("www.pillai.com")
-
-// console.log(typeof myLeads);
+let myLeads = [];
 const inputBtn = document.getElementById("save-btn");
 const delBtn = document.getElementById("del-btn");
 const inputEl = document.getElementById("input-el");
 const unorderList = document.getElementById("ul-el");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-// console.log(leadsFromLocalStorage);
 
+//renders the list items from local storage when webpage loads for the first time
 if (leadsFromLocalStorage) {
-  //leadFromLocalStorage is not null
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
-function strike(completed) {
-  if (completed.style.textDecoration === "line-through") {
-    completed.style.textDecoration = "none";
+
+//strikes off the item from the list upon double click
+function strike(item) {
+  console.log(item)
+  myLeads = JSON.parse(localStorage.getItem("myLeads"));
+
+  for (let i = 0; i < myLeads.length; i++) {
+    let itemObject = myLeads[i].itemName;
+    if (itemObject === item.innerHTML) {
+      myLeads[i].isStrike = !myLeads[i].isStrike;
+      console.log("executed true")
+    }
+  }
+  console.log(myLeads)
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  if (item.style.textDecoration === "line-through") {
+    item.style.textDecoration = "none";
   } else {
-    completed.style.textDecoration = "line-through";
+    item.style.textDecoration = "line-through";
   }
-//   console.log("clicked");
 }
-function render(leads) {
+
+//retriving items from the list
+function render() {
   let listItems = "";
-  for (let i = 0; i < leads.length; i++) {
-    listItems += `<li ondblclick="strike(this)">
-          ${leads[i]}
-          </li>`;
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li style="text-decoration:${myLeads[i].isStrike?"line-through":"none"};" ondblclick="strike(this)">${myLeads[i].itemName}</li>`;
   }
-  //
   unorderList.innerHTML = listItems;
 }
 
-delBtn.addEventListener("dblclick", () => {
-//   console.log("double click");
+//deletes the list 
+delBtn.addEventListener("click", () => {
+
   localStorage.clear();
   myLeads = []; //reassigns the empty array
   render(myLeads); //calling the function with empty array
 });
 
+//takes user input and adding new item in the list 
 inputBtn.addEventListener("click", () => {
-  myLeads.push(inputEl.value); //saved in arr
-  inputEl.value = " ";
+  let newItem = {
+    itemName: inputEl.value,
+    isStrike: false,
+  };
+  myLeads.push(newItem); //saved in arr
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  inputEl.value = "";
   // console.log(typeof myLeads)
-
-  render(myLeads);
-  localStorage.getItem("myLeads");
+  myLeads = JSON.parse(localStorage.getItem("myLeads"));
+  // console.log(itemList);
+  render();
 });
